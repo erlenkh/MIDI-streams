d1081475c6b453fc769190e6e9d665ac88fd0df1
erlenkh <erlenkh@stud.ntnu.no>
2021-02-23 17:46:50 +0100

    implemented functioning orientedTree to represent music temporally. treeToMusic converts an orientedTree to Eueterpeas Music Pitch structure, so that it can be played with MIDI

--- Structure.hs
+++ Structure.hs
@@ -1,21 +1,35 @@
+module Structure
+( OrientedTree (..)
+, Orientation (..)
+, treeToMusic
+) where
+
 import Euterpea
 
 {-
  the grouping structure of a piece, represented as a
- polymorphic tree, with Atoms (Notes or rests) as leaves.
+ polymorphic tree, with Euterpeas primitives (note or rest) as leaves.
  Each branch is labeled with an orientation (chord or line)
 -}
 
---data Atom = Noto (Music Pitch, Volume) | Euterpea.Rest Dur
-
-data Orientation = H | V deriving (Show) -- Horizontal | Vertical
+data Orientation = H | V deriving (Show)  -- Horizontal | Vertical
 
 data OrientedTree a = Val a | Group Orientation [OrientedTree a]
   deriving (Show)
 
+-- converts from a piece of music from orientedTree to Euterpeas 'Music Pitch'
+-- enables us to play the piece as MIDI with built-in Euterpea functions
+treeToMusic :: OrientedTree (Primitive Pitch) -> Music Pitch
+treeToMusic (Val x) = Prim x
+treeToMusic (Group H (x:xs)) = foldl series (treeToMusic x) xs
+  where series acc x = acc :+: treeToMusic x
+treeToMusic (Group V (x:xs)) = foldl parallel (treeToMusic x) xs
+  where parallel acc x = acc :=: treeToMusic x
 
--- figure out a way to deal with primitives..
+{- syntax of creating primitives:
+  prim :: Primitive Pitch
+  prim = Note qn (C,4)
+  prim = Rest qn
+-}
 
-
--- 2 alternatives: create an Event-like variable
--- Use Euterpea..
+-- TODO create slicing abilities like the prefix boyz have done
