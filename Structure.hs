04a9ff2784657f16bd56fc7d823d672d91ae3f7d
Erlend Kilvik Hoff <erlenkh@stud.ntnu.no>
2021-02-24 16:03:00 +0100

    cleanup

--- Structure.hs
+++ Structure.hs
@@ -15,11 +15,8 @@ import Euterpea
  polymorphic tree, with Euterpeas primitives (note or rest) as leaves.
  Each branch is labeled with an orientation H or V corr. to chord or line
 
- Similar structures already exists in euterpea, but re-implemented to allow things like grouping
- by slicing.
-
- lolol
-
+ Similar structures already exists in euterpea, but re-implemented to allow
+ things like grouping by slicing.
 -}
 
 data Orientation = H | V deriving (Show)  -- Horizontal | Vertical
@@ -38,14 +35,12 @@ treeToMusic (Group H (x:xs)) = foldl series (treeToMusic x) xs
 treeToMusic (Group V (x:xs)) = foldl parallel (treeToMusic x) xs
   where parallel acc x = acc :=: treeToMusic x
 
--- get element in tree by integer list
 getElement :: OrientedTree a -> Path -> OrientedTree a
 getElement (Val a) [x] = error "element does not exist"
 getElement tree [] = tree
 getElement (Group o elems) (x:xs) = getElement (elems !! x) xs
 
--- add element to Group in tree
--- input: original tree, tree to add, path to item to add before...
+-- if theres already an element x on path, e is inserted before x
 addToGroup :: OrientedTree a -> OrientedTree a -> Path -> OrientedTree a
 addToGroup tree element [] = tree
 addToGroup (Group o elems) element [idx] = Group o (a ++ [element] ++ b)
@@ -54,8 +49,6 @@ addToGroup (Group o elems) element (x:xs) = Group o newElems
   where (a,e:b) = splitAt x elems
         newElems = a ++ [addToGroup e element xs] ++ b
 
--- remove element to Group in tree
--- input: tree, path to element
 removeFromGroup :: OrientedTree a -> Path -> OrientedTree a
 removeFromGroup tree [] = tree
 removeFromGroup (Group o elems) [x] = Group o (a ++ b)
@@ -64,14 +57,8 @@ removeFromGroup (Group o elems) (x:xs) = Group o newElems
   where (a, e:b) = splitAt x elems
         newElems = a ++ [removeFromGroup e xs] ++ b
 
--- convert Val to Euterpeas music type
 valToMusic :: OrientedTree (Primitive Pitch) -> Music Pitch
 valToMusic (Val x) = Prim (x)
 
-{- syntax of creating primitives:
-  prim :: Primitive Pitch
-  prim = Note qn (C,4)
-  prim = Rest qn
--}
 
 -- TODO create slicing abilities like the prefix boyz have done
