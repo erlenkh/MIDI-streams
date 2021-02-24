e3b8ce1f07a47fc32fbed99e5b9ce8773d6a707e
erlenkh <erlenkh@stud.ntnu.no>
2021-02-24 15:05:50 +0100

    added "addToGroup" and "removeFromGroup"

--- Structure.hs
+++ Structure.hs
@@ -2,8 +2,10 @@ module Structure
 ( OrientedTree (..)
 , Orientation (..)
 , treeToMusic
-, accessElement
+, getElement
 , valToMusic
+, addToGroup
+, removeFromGroup
 ) where
 
 import Euterpea
@@ -22,6 +24,8 @@ data Orientation = H | V deriving (Show)  -- Horizontal | Vertical
 data OrientedTree a = Val a | Group Orientation [OrientedTree a]
   deriving (Show)
 
+type Path = [Int]
+
 -- converts from a piece of music from orientedTree to Euterpeas 'Music Pitch'
 -- enables us to play the piece as MIDI with built-in Euterpea functions
 treeToMusic :: OrientedTree (Primitive Pitch) -> Music Pitch
@@ -31,11 +35,31 @@ treeToMusic (Group H (x:xs)) = foldl series (treeToMusic x) xs
 treeToMusic (Group V (x:xs)) = foldl parallel (treeToMusic x) xs
   where parallel acc x = acc :=: treeToMusic x
 
--- access element in tree by integer list
-accessElement :: OrientedTree a -> [Int] -> OrientedTree a
-accessElement (Val a) [x] = error "element does not exist"
-accessElement tree [] = tree
-accessElement (Group o elems) (x:xs) = accessElement (elems !! x) xs
+-- get element in tree by integer list
+getElement :: OrientedTree a -> Path -> OrientedTree a
+getElement (Val a) [x] = error "element does not exist"
+getElement tree [] = tree
+getElement (Group o elems) (x:xs) = getElement (elems !! x) xs
+
+-- add element to Group in tree
+-- input: original tree, tree to add, path to item to add before...
+addToGroup :: OrientedTree a -> OrientedTree a -> Path -> OrientedTree a
+addToGroup tree element [] = tree
+addToGroup (Group o elems) element [idx] = Group o (a ++ [element] ++ b)
+  where (a, b) = splitAt idx elems
+addToGroup (Group o elems) element (x:xs) = Group o newElems
+  where (a,e:b) = splitAt x elems
+        newElems = a ++ [addToGroup e element xs] ++ b
+
+-- remove element to Group in tree
+-- input: tree, path to element
+removeFromGroup :: OrientedTree a -> Path -> OrientedTree a
+removeFromGroup tree [] = tree
+removeFromGroup (Group o elems) [x] = Group o (a ++ b)
+  where (a, e:b) = splitAt x elems
+removeFromGroup (Group o elems) (x:xs) = Group o newElems
+  where (a, e:b) = splitAt x elems
+        newElems = a ++ [removeFromGroup e xs] ++ b
 
 -- convert Val to Euterpeas music type
 valToMusic :: OrientedTree (Primitive Pitch) -> Music Pitch
