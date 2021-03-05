ef7422168cac58cb57fd2c708199c5be224e78e6
erlenkh <erlenkh@stud.ntnu.no>
2021-03-05 16:05:19 +0100

    - added show instance for orientedTree with function "showTree" - added function "replaceElement"

--- Structure.hs
+++ Structure.hs
@@ -2,14 +2,13 @@ module Structure
 ( OrientedTree (..)
 , Orientation (..)
 , treeToMusic
-, getElement
-, valToMusic
 , addToGroup
 , removeFromGroup
+, replaceElement
 ) where
 
 import Euterpea
-
+import Data.List
 {-
  the grouping structure of a piece, represented as a
  polymorphic tree, with Euterpeas primitives (note or rest) as leaves.
@@ -22,18 +21,40 @@ import Euterpea
 data Orientation = H | V deriving (Show)  -- Horizontal | Vertical
 
 data OrientedTree a = Val a | Group Orientation [OrientedTree a]
-  deriving (Show)
+
+type MusicTree = OrientedTree (Primitive Pitch)
+
+type Padding = Int
+pad 0 = ""
+pad n = " " ++ pad (n-1)
+
+showTree :: Padding -> OrientedTree -> String
+showTree n (Val x) = pad n ++ show x
+showTree n (Group H treez) = "\n" ++ pad n ++ "H\n" ++ horiShow ++ "\n"
+  where horiShow = concat $ map (showTree (n + 2)) treez
+showTree n (Group V treez) = pad n ++ "V: " ++ vertShow ++ "\n"
+  where vertShow = concat $ intersperse "," $ map (showTree 1) treez
+
+instance (Show a) => Show (OrientedTree a) where
+  show x = showTree 0 x
 
 type Path = [Int]
 
+data Choice = Some [Int] | All
+
+type Slice = [Choice]
+
 -- converts from a piece of music from orientedTree to Euterpeas 'Music Pitch'
 -- enables us to play the piece as MIDI with built-in Euterpea functions
-treeToMusic :: OrientedTree (Primitive Pitch) -> Music Pitch
+treeToMusic :: MusicTree -> Music Pitch
 treeToMusic (Val x) = valToMusic (Val x)
 treeToMusic (Group H (x:xs)) = foldl series (treeToMusic x) xs
   where series acc x = acc :+: treeToMusic x
 treeToMusic (Group V (x:xs)) = foldl parallel (treeToMusic x) xs
   where parallel acc x = acc :=: treeToMusic x
+
+valToMusic :: MusicTree -> Music Pitch
+valToMusic (Val x) = Prim (x)
 
 getElement :: OrientedTree a -> Path -> OrientedTree a
 getElement (Val a) [x] = error "element does not exist"
@@ -57,8 +78,13 @@ removeFromGroup (Group o elems) (x:xs) = Group o newElems
   where (a, e:b) = splitAt x elems
         newElems = a ++ [removeFromGroup e xs] ++ b
 
-valToMusic :: OrientedTree (Primitive Pitch) -> Music Pitch
-valToMusic (Val x) = Prim (x)
+replaceElement :: OrientedTree a -> Path -> OrientedTree a -> OrientedTree a
+replaceElement tree path newElement = newTree
+  where newTree = addToGroup (removeFromGroup tree path) newElement path
 
 
+
+
+
+-- TODO Make the tree operations return maybe so we can allow failure..
 -- TODO create slicing abilities like the prefix boyz have done
