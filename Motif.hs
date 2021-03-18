cdda724e55a18c007ababaf99cf8a06f869f16e7
erlenkh <erlenkh@stud.ntnu.no>
2021-03-18 17:51:32 +0100

    dumb shit

--- Motif.hs
+++ Motif.hs
@@ -7,19 +7,53 @@ import Euterpea
 import Scale
 import Data.List
 
+
+
 type Motif = [Primitive Pitch]
 motif :: Motif
 
-motif = [Note en (C,4), Note en (C,4), Note sn (E,4), Note en (B,4), Rest sn]
+motif = [Note en (E,4), Note en (C,4), Note sn (E,4), Note en (B,4), Rest sn]
 
 type ScaleDeg = Int
 
-{-
---inverts around first note
-invert :: Root -> Mode -> Motif -> Motif
+
+-- Diatonic inversion around first note
+invert :: Root -> Mode -> Motif -> [ScaleDeg]
 invert root mode motif =
-  let motifAbs = map primAbs motif
+  let motifSD = map (toScaleDeg root mode) $ getAbsMotif motif
+      invMotifSD = invertSD motifSD
+      invMotifAbs = map (toAbsPitch root mode) $ invMotifSD
+  in invMotifAbs
+
+toScaleDeg root mode = (\(Just x) -> x) . absPitch2ScaleDeg root mode
+toAbsPitch root mode = (\(Just x) -> x) . scaleDeg2AbsPitch root mode
+
+
+
+splitOnRest :: [Primitive Pitch] -> [Primitive Pitch]
+splitOnRest [] = []
+splitOnRest motif  =
+  let notRest (Rest _ ) = False
+      notRest (Note _ _) = True
+  in (takeWhile (notRest) motif)
+
+{-
+replacePitches :: Motif -> [Pitch] -> Motif
+replacePitches motif motifP =
 -}
+
+replacePitch :: Primitive Pitch -> Pitch -> Primitive Pitch
+replacePitch (Rest dur) _ = Rest dur
+replacePitch (Note dur p) newP = (Note dur newP)
+
+invertSD :: [ScaleDeg] -> [ScaleDeg]
+invertSD motifSD =
+  let pitchAxis = cycle $ [head motifSD]
+      invInterSD = map (*(-1)) $ zipWith (-) motifSD pitchAxis
+  in zipWith (+) pitchAxis invInterSD
+
+
+
 reverse :: Motif -> Motif
 reverse motif = Data.List.reverse motif
 
@@ -45,11 +79,10 @@ primAbs2Abs :: Primitive AbsPitch -> Maybe AbsPitch
 primAbs2Abs (Note dur ap) = Just ap
 primAbs2Abs (Rest dur) = Nothing
 
-getAbsIntervals :: Motif -> [AbsPitch]
-getIntervals motif =
+getAbsMotif :: Motif -> [AbsPitch]
+getAbsMotif motif =
   let primAbsMotif = map (primAbs2Abs . primAbs) motif
-      absMotif = map (\(Just x) -> x) $ filter (/= Nothing) $ primAbsMotif
-  in getdif absMotif
+  in map (\(Just x) -> x) $ filter (/= Nothing) $ primAbsMotif
 
-getdif :: [Int] -> [Int]
-getdif x = zipWith (-) (tail x) x
+getDiffs :: [Int] -> [Int]
+getDiffs x = zipWith (-) (tail x) x
