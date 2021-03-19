682190dd07283c76a9c6a184dc9359a3c50cc335
erlenkh <erlenkh@stud.ntnu.no>
2021-03-19 16:38:23 +0100

    cleanup and added some nice tests

--- Motif.hs
+++ Motif.hs
@@ -2,6 +2,7 @@ module Motif
 ( Motif
 , Motif.transpose
 , Motif.reverse
+, Motif.fullReverse
 , Motif.invert
 ) where
 
@@ -9,22 +10,45 @@ import Euterpea
 import Scale
 import Data.List
 
-
 type Motif = [Primitive Pitch]
 
-motif :: Motif
-motif = [Rest sn, Note sn (C,4), Rest sn, Rest sn, Note en (D,4), Rest sn]
+-- TRANSFORMATION IN SCALE CONTEXT ---------------------------------------------
 
-type ScaleDeg = Int
+-- transpose: Transposition of Motif by a given amount of Scale Degrees
+transpose :: Root -> Mode -> Motif -> ScaleDeg -> Motif
+transpose root mode motif deg = map (primTrans root mode deg) motif
+
+-- reverse: Reversion of pitch sequence but not durations or rests
+reverse :: Motif -> Motif
+reverse motif =
+  let motifP = getPitches motif -- removes rests
+      revMotifP = Data.List.reverse motifP
+  in replacePitches motif revMotifP
+
+-- fullReverse: Full reversion of motif
+fullReverse :: Motif -> Motif
+fullReverse motif = Data.List.reverse motif
 
 -- invert: Diatonic inversion of Motif around first note, preserving Rests
 invert :: Root -> Mode -> Motif -> Motif
 invert root mode motif =
-  let motifSD = map (toScaleDeg root mode) $ getAbsMotif motif -- to scale deg
-      invMotifSD = invertSD motifSD -- invert scale deg
-      invMotifP = map (pitch . toAbsPitch root mode) $ invMotifSD -- to pitches
+  let motifP = getPitches motif -- removes rests
+      motifSD = map (toScaleDeg root mode) $ map (absPitch) motifP
+      invMotifSD = invertSD motifSD
+      invMotifP = map (pitch . toAbsPitch root mode) $ invMotifSD
       invMotif = replacePitches motif invMotifP -- replace inv pitches in motif
   in invMotif
+
+-- HELPER FUNCTIONS ------------------------------------------------------------
+
+-- primTrans: Transposition of a Primitive Pitch by a given amt of Scale Degrees
+-- NB: fails if pitch is outside of absPitch (0,127) i.e MIDI scale (use Maybe)
+primTrans :: Root -> Mode -> ScaleDeg -> Primitive Pitch -> Primitive Pitch
+primTrans _ _ _ (Rest dur) = Rest dur
+primTrans root mode steps (Note dur p) =
+  let transposedSD =  (toScaleDeg root mode (absPitch p)) + steps
+      transposedAP = toAbsPitch root mode transposedSD
+  in Note dur (pitch transposedAP)
 
 -- invertSD: Diatonic inversion of sequence of Scale Degrees around first note
 invertSD :: [ScaleDeg] -> [ScaleDeg]
@@ -33,30 +57,8 @@ invertSD motifSD =
       invInterSD = map (*(-1)) $ zipWith (-) motifSD pitchAxis
   in zipWith (+) pitchAxis invInterSD
 
--- reverse: Reversion of Motif
-reverse :: Motif -> Motif
-reverse motif = Data.List.reverse motif
-
--- reverse: Transposition of Motif by a given amount of Scale Degrees
-transpose :: Root -> Mode -> Motif -> ScaleDeg -> Motif
-transpose root mode motif deg = map (primTrans root mode deg) motif
-
--- primTrans: Transposition of a Primitive Pitch by a given amt of Scale Degrees
--- NB: fails if pitch is outside of absPitch (0,127) i.e MIDI scale (use Maybe)
-primTrans :: Root -> Mode -> ScaleDeg -> Primitive Pitch -> Primitive Pitch
-primTrans root mode deg (Rest dur) = Rest dur
-primTrans root mode deg (Note dur p) =
-  let fullMIDIScale = createFullMIDIScale root mode
-      Just aPIdx = elemIndex (absPitch p) fullMIDIScale
-      transAPIdx = aPIdx + deg
-      transAP = fullMIDIScale !! transAPIdx
-  in Note dur (pitch transAP)
-
-
--- HELPER FUNCTIONS ------------------------------------------------------------
-
 -- replacePitches: takes a sequence of Pitches and a Motif and changes the
--- Pitches while preserving the Rests as they were.
+-- Pitches while preserving the Rests and durations.
 replacePitches :: Motif -> [Pitch] -> Motif
 replacePitches motifM motifP = concat $ splitAndReplace motifM motifP
 
@@ -68,34 +70,37 @@ splitAndReplace motif pitches  = filter (/= []) $
       newPrimPs = zipWith replacePitch primPs (fst splitPs)
       second = dropWhile (notRest) motif
       rest = head second
-  in case length second of
-        1 -> newPrimPs : [[rest]]
-        0 -> newPrimPs : []
-        otherwise ->
-          newPrimPs : [rest] : splitAndReplace (tail second) (snd splitPs)
+  in newPrimPs : [rest] : splitAndReplace (drop 1 second) (snd splitPs)
 
 replacePitch :: Primitive Pitch -> Pitch -> Primitive Pitch
 replacePitch (Rest dur) _ = Rest dur
 replacePitch (Note dur p) newP = (Note dur newP)
 
-primAbs :: Primitive Pitch -> Primitive AbsPitch
-primAbs (Note dur p) = (Note dur (absPitch p))
-primAbs (Rest dur) = Rest dur
+getMaybePitch :: Primitive Pitch -> Maybe Pitch
+getMaybePitch (Note _ p) = Just p
+getMaybePitch (Rest _) = Nothing
 
-primAbs2Abs :: Primitive AbsPitch -> Maybe AbsPitch
-primAbs2Abs (Note dur ap) = Just ap
-primAbs2Abs (Rest dur) = Nothing
+getPitches :: Motif -> [Pitch]
+getPitches motif =
+  let pitches = map getMaybePitch motif
+  in map (\(Just x) -> x) $ filter (/= Nothing) $ pitches
 
-getAbsMotif :: Motif -> [AbsPitch]
-getAbsMotif motif =
-  let primAbsMotif = map (primAbs2Abs . primAbs) motif
-  in map (\(Just x) -> x) $ filter (/= Nothing) $ primAbsMotif
-
-getDiffs :: [Int] -> [Int]
-getDiffs x = zipWith (-) (tail x) x
-
-toScaleDeg root mode = (\(Just x) -> x) . absPitch2ScaleDeg root mode
-toAbsPitch root mode = (\(Just x) -> x) . scaleDeg2AbsPitch root mode
+toScaleDeg root mode = (\(Just x) -> x) . Scale.absPitch2ScaleDeg root mode
+toAbsPitch root mode = (\(Just x) -> x) . Scale.scaleDeg2AbsPitch root mode
 
 notRest (Rest _ ) = False
 notRest (Note _ _) = True
+
+-- TESTING ---------------------------------------------------------------------
+
+motif :: Motif
+motif = [Rest sn, Note en (C,4), Rest sn, Note sn (C,4), Note en (E,4), Note en (B,4), Rest sn]
+
+motif_inv = Motif.invert C Major motif
+motif_trans2 = Motif.transpose C Major motif 2
+motif_rev = Motif.reverse motif
+
+test_all = toM $ concat [motif, motif_inv, motif_trans2, motif_rev]
+test_trans = toM $ concat $ map (Motif.transpose C Major motif) [0..8]
+
+toM motif = line $ map (\x -> Prim (x)) $ motif
