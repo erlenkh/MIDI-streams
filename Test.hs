c109303ff1e2fcac460e66523e29f21567696b98
erlenkh <erlenkh@stud.ntnu.no>
2021-03-23 14:55:12 +0100

    some testing

--- Test.hs
+++ Test.hs
@@ -2,6 +2,7 @@
 import Scale
 import Structure
 import Euterpea
+import qualified Transform as T
 
 chordPart :: OrientedTree (Primitive Pitch)
 chordPart =
@@ -20,9 +21,20 @@ chordPart =
                 Val (Rest hn)
                 ]
 
-
-
 song :: OrientedTree (Primitive Pitch)
 song = Group H [chordPart, chordPart]
 
 newSong = replaceElement song [0,1] $ Val (Note qn (E,4))
+
+motif :: T.Motif
+motif = [Note qn (C,4), Note qn (D,4), Note qn (E,4), Note qn (B,4)]
+
+motif_inv = T.invert C Major motif
+motif_trans2 = T.transpose C Major motif 2
+motif_rev = T.reverse motif
+
+mel :: MusicTree
+mel = Group H $ map (toGroup H) [motif, motif_inv, motif, motif_rev]
+
+mel2 = applyFunction (T.replacePitch ((G,3) :: Pitch)) [Some [1], Some [3]] mel
+mel3 = applyFunction (T.replacePitch ((C,4) :: Pitch)) [Some [3], Some [3]] mel2
