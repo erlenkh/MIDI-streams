b6992b31e48c50f51b2635bd65914ca7b3a1ee09
[erlenkh] <[erlendkhoff@gmail.com]>
2021-03-31 19:37:35 +0200

    valToMusic: now adds a volume of 75 to pitch new slicing: atVoices, atPeriods, atMotifs fix pt2Tree reduced tempo on p added first attempt at "cellevevet"

--- Composer.hs
+++ Composer.hs
@@ -23,13 +23,14 @@ type MusicTree = OrientedTree (Primitive Pitch)
 
 -- converts from a piece of music from orientedTree to Euterpeas 'Music Pitch'
 -- enables us to play the piece as MIDI with built-in Euterpea functions
-treeToMusic :: MusicTree -> Music Pitch
+treeToMusic :: MusicTree -> Music (Pitch, Volume)
 treeToMusic (Val x) = valToMusic (Val x)
 treeToMusic (Group H trees) = line (map treeToMusic trees)
 treeToMusic (Group V trees) = chord (map treeToMusic trees)
 
-valToMusic :: MusicTree -> Music Pitch
-valToMusic (Val x) = Prim (x)
+valToMusic :: MusicTree -> Music (Pitch, Volume)
+valToMusic (Val (Note dur p)) = Prim ((Note dur (p, 75)))
+valToMusic (Val (Rest dur)) = Prim (Rest dur)
 
 type MusicPT = PrefixTree GT (Slice -> Slice)
 
@@ -40,9 +41,10 @@ type MusicPT = PrefixTree GT (Slice -> Slice)
 -- should they add? i.e. atVoices[0,1] . atVoices[2] = atVoices [0,1,2]?
 -- right now atVoices[0,1] . atVoices[2] = atVoices [0,1]
 
-atPeriods, atMotifs :: [Int] -> Slice -> Slice
-atPeriods selection [_ , motifs] = [Some selection, motifs]
-atMotifs selection [periods, _] = [periods, Some selection]
+atVoices, atPeriods, atMotifs :: [Int] -> Slice -> Slice
+atVoices selection [a, b, c] = [Some selection, b, c]
+atPeriods selection [a, b, c] = [a, Some selection, c]
+atMotifs selection [a, b, c] = [a, b, Some selection ]
 
 -- GROUP TRANSFORMATIONS: ------------------------------------------------------
 type GT = MusicTree -> MusicTree
@@ -60,6 +62,7 @@ weak = toGT $ T.weakCadence C Major
 ro = toGT . T.reorder
 insert new old = new
 ext = toGT . T.extend
+mlSD x = toGT $ T.movelastSD C Major x
 
 -- TRANSFORMATIVE INSTRUCTIONS -------------------------------------------------
 
@@ -81,19 +84,18 @@ toTI levels (strans, gtrans) =
 
 pt2Tree :: MusicPT -> MusicTree
 pt2Tree pt =
-  let levels = [All, All]
-      values = getAllValues $ fmap ($ levels) testPT
-      paths = getAllPaths testPT
+  let levels = [All, All, All]
+      values = getAllValues $ fmap ($ levels) pt
+      paths = getAllPaths pt
   in tis2Tree $ map (toTI levels) $ zip paths values
 
 
 -- TESTING ZONE: ---------------------------------------------------------------
 
-p tree = playDev 2 (treeToMusic tree) --quick play
+p tree = playDevS 2 $ tempo 0.80 $ (treeToMusic tree) --quick play
 
 motif, motif2, motif3 :: T.Motif
-motif = [Note qn (C,4), Note qn (D,4), Note qn (E,4), Note qn (B,4)]
-motif2 = [Note qn (C,4), Note qn (C,4), Note qn (B,4), Note qn (E,4)]
+motif2 = [Note qn (C,4), Note qn (C,4), Note qn (B,3), Note qn (E,4)]
 motif3 = [Note sn (A,4), Note sn (A,4), Note qn (B,4), Note qn (C,4)]
 
 base m = (Group H $ map (toGroup H) $  replicate 4 m) :: MusicTree
@@ -103,12 +105,43 @@ period = applyGT [Some[1]] (weak. transp (-2)) .
 m1, m2, m3, m4 :: MusicTree
 m1 = toGroup H [Note hn (C,3), Note qn (E,3), Note qn (F,3)]
 m2 = toGroup H [Note qn (C,2), Note hn (D,2), Note qn (E,2)]
-m3 = toGroup H [Note qn (C,4), Note qn (D,4), Note hn (B,4)]
 m4 = toGroup H [Note qn (C,4), Note en (D,4), Note en (B,4)]
 
+
+motif = [Note qn (C,4), Rest en, Note en (D,4), Note qn (E,4), Note qn (B,4)]
+m3 = toGroup H [Note qn (C,4), Note qn (D,4), Note hn (B,4)]
+
+cvMotif :: MusicTree
+cvMotif = toGroup H [Note en (C,4), Note en (C,4), Note en (C,4), Note en (C,4)]
+cvMotif2 = toGroup H [Rest sn, Note en (C,4), Note en (C,4), Note en (C,4), Note sn (C,4)]
+
+-- first attempt at "cellevevet"
+cv :: MusicPT
+cv = Node (atPeriods [0,1,2,3,4,5,6,7]) [
+         Node (atVoices [0] ) [
+            Leaf (atMotifs [0,1,2,3,4,5,6,7]) (insert $ transp (-7) cvMotif)
+         ,  Leaf (atMotifs [2,3]) (transp (-2))
+         ,  Leaf (atMotifs [4,5]) (transp (-4))
+         ,  Leaf (atMotifs [1,3]) (mlSD (-1))
+         ,  Leaf (atMotifs [6]) (transp 1)
+         ,  Leaf (atMotifs [7]) (transp(-3))
+        ]
+     ,  Node (atVoices [1]) [
+          Leaf (atMotifs [0,1,2,3,4,5,6,7]) (insert $ cvMotif2)
+        , Leaf (atPeriods [3,5,7] . atMotifs [7]) (transp (-1))
+        ]
+     ,  Leaf (atVoices [0,1] . atPeriods [4,5,6,7,8] . atMotifs [0,1,2,3,4,5,6,7]) (transp (-3))
+     ]
+
+-- need to combine multiplegroups and act on them like 1 !!
+
+{-
 testPT :: MusicPT
-testPT = Node (atPeriods [0,1]) [
-            Leaf (atMotifs [0,1,2,3]) (insert $ toGroup H motif)
+testPT = Node (atPeriods [0,1,2,3]) [
+            Node (atMotifs [0,1,2,3])[
+              Leaf (atPeriods [0,1]) (insert $ toGroup H motif)
+            , Leaf (atPeriods [2,3]) (insert $ toGroup H motif2)
+            ]
          ,  Leaf (atMotifs [1,3]) (ext 1 . giveR m3 . inv)
          ,  Node (atPeriods [1]) [
               Leaf (atMotifs [0,1,2,3]) (transp 2)
@@ -116,7 +149,7 @@ testPT = Node (atPeriods [0,1]) [
          ,  Leaf (atMotifs [1]) weak
          ,  Leaf (atMotifs [3]) strong
          ]
-
+-}
 testTIs = [ TI [All, All] (insert $ toGroup H motif),
             TI [Some[1,3], Some[1,3]] inv,
             TI [All, Some[1]] (ext 1 . giveR m3 . weak . transp (-2)),
@@ -178,7 +211,7 @@ makeStartingTree tis =
 
 toDefaultOrientedTree :: TreeShape -> MusicTree
 toDefaultOrientedTree =
-  go $ repeat (Group H)
+  go $ [Group V, Group H, Group H] ++ repeat (Group H) -- left is top
  where
   go (c : cs) TLeaf      = Group H [Val $ Rest sn]
   go (c : cs) (TAll  t ) = c [go cs t]
