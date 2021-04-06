cd210f62092f217e359aa71df8a7fff0297db006
[erlenkh] <[erlendkhoff@gmail.com]>
2021-04-06 11:08:18 +0200

    removed some test. recreation of cellevevet. (problem: doesnt really capture songs essence..) (todo: understanding of chords,and how chords are played in time...)

--- Composer.hs
+++ Composer.hs
@@ -63,6 +63,7 @@ ro = toGT . T.reorder
 insert new old = new
 ext = toGT . T.extend
 mlSD x = toGT $ T.movelastSD C Major x
+ct = toGT . T.cTrans
 
 -- TRANSFORMATIVE INSTRUCTIONS -------------------------------------------------
 
@@ -111,63 +112,39 @@ m4 = toGroup H [Note qn (C,4), Note en (D,4), Note en (B,4)]
 motif = [Note qn (C,4), Rest en, Note en (D,4), Note qn (E,4), Note qn (B,4)]
 m3 = toGroup H [Note qn (C,4), Note qn (D,4), Note hn (B,4)]
 
+-- simplified version of "cellevevet"
+
 cvMotif :: MusicTree
 cvMotif = toGroup H [Note en (C,4), Note en (C,4), Note en (C,4), Note en (C,4)]
 cvMotif2 = toGroup H [Rest sn, Note en (C,4), Note en (C,4), Note en (C,4), Note sn (C,4)]
 
--- first attempt at "cellevevet"
-cv :: MusicPT
-cv = Node (atPeriods [0,1,2,3,4,5,6,7]) [
-         Node (atVoices [0] ) [
-            Leaf (atMotifs [0,1,2,3,4,5,6,7]) (insert $ transp (-7) cvMotif)
-         ,  Leaf (atMotifs [2,3]) (transp (-2))
-         ,  Leaf (atMotifs [4,5]) (transp (-4))
-         ,  Leaf (atMotifs [1,3]) (mlSD (-1))
-         ,  Leaf (atMotifs [6]) (transp 1)
-         ,  Leaf (atMotifs [7]) (transp(-3))
+cv = Node (atMotifs [0,1,2,3,4,5,6,7]) [
+        Node (atVoices [0]) [
+           Leaf (atPeriods [0,1,2,3,4,5,6,7,8]) (insert $ transp (-7) cvMotif)
+        ,  Node (atPeriods [0,1,2,3,7,8]) [
+              Leaf (atMotifs [2,3]) (transp (-2))
+           ,  Leaf (atMotifs [4,5]) (transp (-4))
+           ,  Leaf (atMotifs [1,3]) (mlSD (-1))
+           ,  Leaf (atMotifs [6]) (transp 1)
+           ,  Leaf (atMotifs [7]) (transp(-3))
+           ]
+        ,  Node (atPeriods[4,5,6]) [
+             Leaf (atMotifs [2,3]) (transp (-1))
+           , Leaf (atMotifs [6,7]) (ct (1))
+           ]
         ]
-     ,  Node (atVoices [1]) [
-          Leaf (atMotifs [0,1,2,3,4,5,6,7]) (insert $ cvMotif2)
-        , Leaf (atPeriods [3,5,7] . atMotifs [7]) (transp (-1))
+     ,  Node (atVoices [1])[
+           Leaf (atPeriods[0,1,2,3,4,5,6,7,8]) (insert $ cvMotif2)
+        ,  Leaf (atPeriods[3] .atMotifs [7]) (transp (-1))
+        ,  Node (atPeriods [4,5,6]) [
+              Leaf (atMotifs [0,1]) (transp (-4))
+           ,  Leaf (atMotifs [2,3,4,5,6,7]) (transp (-5))
+           ]
         ]
-     ,  Leaf (atVoices [0,1] . atPeriods [4,5,6,7,8] . atMotifs [0,1,2,3,4,5,6,7]) (transp (-3))
      ]
 
 -- need to combine multiplegroups and act on them like 1 !!
 
-{-
-testPT :: MusicPT
-testPT = Node (atPeriods [0,1,2,3]) [
-            Node (atMotifs [0,1,2,3])[
-              Leaf (atPeriods [0,1]) (insert $ toGroup H motif)
-            , Leaf (atPeriods [2,3]) (insert $ toGroup H motif2)
-            ]
-         ,  Leaf (atMotifs [1,3]) (ext 1 . giveR m3 . inv)
-         ,  Node (atPeriods [1]) [
-              Leaf (atMotifs [0,1,2,3]) (transp 2)
-            ]
-         ,  Leaf (atMotifs [1]) weak
-         ,  Leaf (atMotifs [3]) strong
-         ]
--}
-testTIs = [ TI [All, All] (insert $ toGroup H motif),
-            TI [Some[1,3], Some[1,3]] inv,
-            TI [All, Some[1]] (ext 1 . giveR m3 . weak . transp (-2)),
-            TI [All, Some[3]] (ext 1. giveR m3 .strong),
-            TI [Some [2,3], All] (transp 3),
-            TI [Some [4,5], All] (inv . rev . transp (-2))
-           ]
-
-lol = [TI [Some[0,1,2,3]] (insert para),
-       TI [All, Some[1], All] (transp (-14)),
-       TI [All, Some[1], Some[1,3]] (rev),
-       TI [Some[2,3], Some[0], All] (transp 2),
-       TI [All, All, Some[3]] (transp (-1))
-       ]
-
-para = Group V [chords, applyGT [All] (giveR m4 .toGroup H . fromGroup . transp 7) chords]
-
-startingTree = base motif
 
 chords :: OrientedTree (Primitive Pitch)
 chords =
