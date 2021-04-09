5e1110258def519803aa0a34342434677fc883db
erlenkh <erlenkh@stud.ntnu.no>
2021-04-09 18:41:07 +0200

    good cellevevet, very generalized toCV

--- Composer.hs
+++ Composer.hs
@@ -130,15 +130,14 @@ getSlices pt = map slc $ pt2TIs pt
 
 -- TESTING ZONE: ---------------------------------------------------------------
 
-
 p tree = playDevS 6 $ tempo 0.80 $ (treeToMusic tree) --quick play
 
 mkChord pitch mode dur = map (\p -> Note dur p) $ pitches $ getTriad pitch mode
 
 mc p o m = insert $ toGroup V $ mkChord (p,o) m hn
 
-cv_chords = Node (atPeriods [0,1,2,3,4,5,6,7]) [
-              Node (atPeriods [0,1,2,3,7]) [
+cv      = Node (atPeriods [0,1,2,3,4,5,6,7]) [
+              Node (atPeriods [0,1,2,6,7]) [
                 Node (atMeasures [0,1]) [
                     Leaf (atPhrases [0]) (mc C 3 Major)
                 ,   Leaf (atPhrases [1]) (mc A 2 Minor)
@@ -148,10 +147,11 @@ cv_chords = Node (atPeriods [0,1,2,3,4,5,6,7]) [
                     ,   Leaf (atMeasures [1]) (mc G 2 Major)
                     ]
                 ]
-                , Leaf (atMeasures [0,1]) (toCV)
+                , Leaf (atMeasures [0,1]) (toCV1)
                 , Leaf (atPhrases [0,1] . atMeasures [1] . atDepth 3 [0]) (mlSD (-1))
+                , Leaf (atPeriods [2,7] . atPhrases [3] . atMeasures [1] . atDepth 3 [1]) (transp (-1))
             ]
-            , Node (atPeriods [4,5,6]) [
+            , Node (atPeriods [3,4,5,8,9,10]) [
                 Node (atMeasures [0,1]) [
                     Leaf (atPhrases [0]) (mc F 2 Major)
                 ,   Leaf (atPhrases [2]) (mc A 2 Minor)
@@ -161,22 +161,48 @@ cv_chords = Node (atPeriods [0,1,2,3,4,5,6,7]) [
                     ,   Leaf (atMeasures [1]) (mc E 2 Major)
                     ]
                 ]
-              , Leaf (atMeasures [0,1]) (toCV)
-
+              , Leaf (atMeasures [0,1]) (toCV2)
               ]
-            ]
+          ]
 
+toCV' :: [Pitch] -> [Pitch] -> MusicTree
+toCV' n1 n2 =
+  let f = L.intersperse (Val $ Rest sn) . concat . replicate 4
+      g1 = Group H $ f [toGroup V $ map (\p -> Note sn p) n1] ++ [Val $ Rest sn]
+      g2 = Group H $ (Val $ Rest sn) : f [toGroup V $ map (\p -> Note sn p) n2]
+  in Group V [g1, g2]
 
+toCV1 tree =
+  let notes = T.getPitches $ flatten tree
+  in toCV' [head notes] [(C,4)]
+
+toCV2 tree =
+  let notes = T.getPitches $ flatten tree
+  in toCV' [head notes] (tail notes)
+
+{-
 toCV :: MusicTree -> MusicTree
 toCV tree =
   let notes = flatten tree
-      voice1 = [head notes] :: T.Motif
+      voice1 = [T.replaceDuration en $ head notes] :: T.Motif
       voice2 = [Note en (C,4)] :: T.Motif
       f = concat . replicate 4
   in Group V [  Group H (map toVal $ f voice1)
              ,  Group H ( map toVal $ T.fit 0.5 $ (Rest sn :: Primitive Pitch) : f voice2)
              ]
 
+toCV2 :: MusicTree -> MusicTree
+toCV2 tree =
+ let notes = (map (T.replaceDuration en) $ flatten tree)
+     voice1 = [head notes] :: T.Motif
+     stack = tail notes
+     voice2 = toGroup V $ (stack)
+     voice3 = toGroup V $ map (T.replaceDuration sn) $ stack
+     f x = concat . replicate x
+ in Group V [  Group H (map toVal $ f 4 voice1)
+            ,  Group H ( (Val $ Rest sn :: MusicTree ) : f 3 [voice2] ++ [voice3])
+            ]
+-}
 {-
 toCV2 :: MusicTree -> MusicTree
 toCV2 (Group V notes) =
