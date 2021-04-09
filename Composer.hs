6c2fdcfb81cc3941f4e8e481187fa840bec3811d
erlenkh <erlenkh@stud.ntnu.no>
2021-04-09 16:04:38 +0200

    better slice construction: now a general function atDepth, where top level is depth 0 etc. also atLevel, so you can get bottom level aswell.
    Other changes: 
    flattenSTs, when a list of slice transfomrations are flattened into a 
    slice, they are applied til a list of All with same length as max depth 
    of sts. This means that we can cleanly add deeper levels as we go from 
    left to right in prefixtree, and perform functions on these. useful when 
    giving a chord a playing style... 
    
    cv with chords basically works

--- Composer.hs
+++ Composer.hs
@@ -16,6 +16,7 @@ import Structure
 import Euterpea
 import qualified Transform as T
 import Chord
+import qualified Data.List as L
 -- MUSIC TREES ------------------------------------------------------------------
 
 type MusicTree = OrientedTree (Primitive Pitch)
@@ -36,10 +37,23 @@ type MusicPT = PrefixTree GT (Slice -> Slice)
 -- SLICE CONSTRUCTION ----------------------------------------------------------
 
 -- slice construction: allows the composition of (Slice -> Slice)
--- examples that apply to "testTree": (need to be generalized)
 -- should they add? i.e. atVoices[0,1] . atVoices[2] = atVoices [0,1,2]?
 -- right now atVoices[0,1] . atVoices[2] = atVoices [0,1]
 
+atPeriods  = atDepth 0
+atPhrases = atDepth 1
+atMeasures = atDepth 2
+
+--crashes if lvl >= length slice (might fix with Maybe)
+atLevel :: Int -> [Int] -> Slice -> Slice
+atLevel lvl selection slice =
+  let (first, second) = splitAt lvl (reverse slice)
+  in reverse $ first ++ [Some selection] ++ tail second
+
+atDepth :: Int -> [Int] -> Slice -> Slice
+atDepth lvl selection slice =
+  let (first, second) = splitAt lvl slice
+  in first ++ [Some selection] ++ tail second
 
 -- GROUP TRANSFORMATIONS: ------------------------------------------------------
 type GT = MusicTree -> MusicTree
@@ -64,7 +78,12 @@ invGT = applySF $ T.invert C Major
 
 -- TRANSFORMATIVE INSTRUCTIONS -------------------------------------------------
 
-data TI = TI { slc :: Slice, gt :: GT} -- Transformative Instruction
+data TI = TI { slc :: Slice, gt :: GT}  -- Transformative Instruction
+
+
+-- TRANSFORMATIVE INSTRUCTION 2 MUSIC TREE -------------------------------------
+tis2Tree :: [TI] -> MusicTree
+tis2Tree instructions = applyTIs instructions (makeStartingTree instructions)
 
 applyTIs :: [TI] -> MusicTree -> MusicTree
 applyTIs instructions startingTree =
@@ -73,26 +92,44 @@ applyTIs instructions startingTree =
 applyTI :: TI -> MusicTree -> MusicTree
 applyTI (TI slice gt) tree = applyGT slice gt tree
 
-tis2Tree :: [TI] -> MusicTree
-tis2Tree instructions = applyTIs instructions (makeStartingTree instructions)
+-- PREFIXTREE 2 TRANSFORMATIVE INSTRUCTIONS ------------------------------------
 
-toTI :: Slice -> ([Slice -> Slice], GT) -> TI
-toTI levels (strans, gtrans) =
-   TI {slc = foldl(\acc f -> f acc) levels strans, gt = gtrans }
+flattenSTs :: Slice -> [Slice -> Slice] -> Slice
+flattenSTs levels sts = foldl(\acc f -> f acc) levels sts
 
-pt2Tree :: MusicPT -> MusicTree
-pt2Tree pt =
-  let levels = [All, All, All]
-      values = getAllValues $ fmap ($ levels) pt
-      paths = getAllPaths pt
-  in tis2Tree $ map (toTI levels) $ zip paths values
+tightFlattenSts :: [Slice -> Slice] -> Slice
+tightFlattenSts sts = flattenSTs (getAlls sts) sts
 
+getAlls sts = replicate ((getMaxDepth sts) + 1) All
 
+toTI :: ([Slice -> Slice], GT) -> TI
+toTI (sts, gtrans) = TI {slc = tightFlattenSts sts, gt = gtrans}
+
+pt2TIs :: MusicPT -> [TI]
+pt2TIs pt =
+  let stss = getAllPaths pt
+      maxLevels = getAlls $ concat stss
+      gts = getAllValues $ fmap ($ maxLevels) pt
+  in map (toTI) $ zip stss gts
+
+pt2MT :: MusicPT -> MusicTree
+pt2MT pt = tis2Tree $ pt2TIs pt
+
+getMaxDepth :: [Slice -> Slice] -> Int
+getMaxDepth sts = maximum $ map getDepth sts
+
+-- a song cannot have more that 666 hierarchical levels, should be generalized
+getDepth :: (Slice -> Slice) -> Int
+getDepth sTrans = maximum $ L.findIndices (isSome) $ sTrans $ replicate (666) All
+
+isSome (Some xs) = True
+isSome _  = False
+
+getSlices pt = map slc $ pt2TIs pt
+
+test = applyTIs (pt2TIs lol) (pt2MT dt)
 -- TESTING ZONE: ---------------------------------------------------------------
 
-atPeriods selection [a,b,c] = [Some selection, b, c]
-atPhrases selection [a,b,c] = [a, Some selection,c]
-atMeasures selection [a,b,c] = [a,b, Some selection]
 
 p tree = playDevS 6 $ tempo 0.80 $ (treeToMusic tree) --quick play
 
@@ -100,22 +137,48 @@ mkChord pitch mode dur = map (\p -> Note dur p) $ pitches $ getTriad pitch mode
 
 mc p o m = insert $ toGroup V $ mkChord (p,o) m hn
 
+cv_chords = Node (atPeriods [0,1,2,3,4,5,6,7]) [
+              Node (atPeriods [0,1,2,3,7]) [
+                Node (atMeasures [0,1]) [
+                    Leaf (atPhrases [0]) (mc C 3 Major)
+                ,   Leaf (atPhrases [1]) (mc A 2 Minor)
+                ,   Leaf (atPhrases [2]) (mc F 2 Major)
+                ,   Node (atPhrases [3]) [
+                        Leaf (atMeasures [0]) (mc D 3 Minor)
+                    ,   Leaf (atMeasures [1]) (mc G 2 Major)
+                    ]
+                ]
+                , Leaf (atMeasures [0,1]) (toCV)
+                , Leaf (atPhrases [0,1] . atMeasures [1] . atDepth 3 [0]) (mlSD (-1))
+            ]
+            , Node (atPeriods [4,5,6]) [
+                Node (atMeasures [0,1]) [
+                    Leaf (atPhrases [0]) (mc F 2 Major)
+                ,   Leaf (atPhrases [2]) (mc A 2 Minor)
+                ,   Leaf (atPhrases [3]) (mc A 2 Major)
+                ,   Node (atPhrases [1]) [
+                        Leaf (atMeasures [0]) (insert $ toGroup V [Note hn (E,2), Note hn (A,2), Note hn ((B,2))])
+                    ,   Leaf (atMeasures [1]) (mc E 2 Major)
+                    ]
+                ]
+              , Leaf (atMeasures [0,1]) (toCV)
 
-cv_chords = Node (atPeriods [0]) [
-              Node (atMeasures[0,1]) [
-                  Leaf (atPhrases [0]) (mc C 3 Major)
-              ,   Leaf (atPhrases [1]) (mc A 2 Minor)
-              ,   Leaf (atPhrases [2]) (mc F 2 Major)
-              ,   Node (atPhrases [3]) [
-                      Leaf (atMeasures[0]) (mc D 3 Minor)
-                  ,   Leaf (atMeasures[1]) (mc G 2 Major)
-                  ]
               ]
-            , Leaf (atPhrases [0,1,2,3]) (toCV)
-            , Node (atMeasures[1]) [
-                Leaf (atPhrases [0,1] . atMeasures[1]) (mlSD (-1))
             ]
-            ]
+
+dt = Node (atDepth 0 [0])[
+        Leaf (atDepth 1 [0,1]) (mc C 3 Major)
+     ,  Leaf (atDepth 1 [0]) (toCV)
+     , Node (atDepth 1 [0]) [
+        Leaf (atDepth 2 [1]) (transp 2)
+        ]
+    ]
+lol = Node (atDepth 0 [0]) [
+        Node (atDepth 1 [0]) [
+            Leaf (atDepth 2 [0]) (transp 2)
+          ]
+      ]
+
 
 toCV :: MusicTree -> MusicTree
 toCV (Group V notes) =
@@ -126,83 +189,22 @@ toCV (Group V notes) =
   in Group V [  Group H (map toVal $ f voice1)
              ,  Group H ( map toVal $ T.fit 0.5 $ (Rest sn :: Primitive Pitch) : f voice2)
              ]
+{-
+toCV2 :: MusicTree -> MusicTree
+toCV2 (Group V notes) =
+ let n = replaceDurations [sn,sn,sn] (fromGroup V) notes
+     voice1 = [T.replaceDuration (sn) $ head notes]
+     voice2 = [Group V $ map (replaceDuration (sn)) $ tail notes]
+     f = concat . replicate 4
+ in Group V [  Group H ( f voice1)
+            ,  Group H ( (Rest sn :: Primitive Pitch) : f voice2)
+            ]
+-}
+tes = [Val $ Note en (C,4), Val $ Note en (C,3)]
+
 
 toVal = (\x -> Val x)
 
-{-
-toCV :: MusicTree -> MusicTree
-toCV (Group V notes) =
-  let Val (Note dur root) = head notes
-      rhytmicroot = [Note sn root, Note sn (C,4)] :: T.Motif
-  in Group H (map (\x -> Val x) $ concat $ replicate 4 rhytmicroot)
--}
-
--- simplified version of "cellevevet"
-
-{-
-atVoices, atPeriods, atMotifs :: [Int] -> Slice -> Slice
-atVoices selection [a, b, c] = [Some selection, b, c]
-atPeriods selection [a, b, c] = [a, Some selection, c]
-atMotifs selection [a, b, c] = [a, b, Some selection ]
-
-cvMotif :: MusicTree
-cvMotif = toGroup H [Note en (C,4), Note en (C,4), Note en (C,4), Note en (C,4)]
-cvMotif2 = toGroup H [Rest sn, Note en (C,4), Note en (C,4), Note en (C,4), Note sn (C,4)]
-
-cv = Node (atMotifs [0,1,2,3,4,5,6,7]) [
-        Node (atVoices [0]) [
-           Leaf (atPeriods [0,1,2,3,4,5,6,7,8]) (insert $ transp (-7) cvMotif)
-        ,  Node (atPeriods [0,1,2,3,7,8]) [
-              Leaf (atMotifs [2,3]) (transp (-2))
-           ,  Leaf (atMotifs [4,5]) (transp (-4))
-           ,  Leaf (atMotifs [1,3]) (mlSD (-1))
-           ,  Leaf (atMotifs [6]) (transp 1)
-           ,  Leaf (atMotifs [7]) (transp(-3))
-           ]
-        ,  Node (atPeriods[4,5,6]) [
-             Leaf (atMotifs [2,3]) (transp (-1))
-           , Leaf (atMotifs [6,7]) (ct (1))
-           ]
-        ]
-     ,  Node (atVoices [1])[
-           Leaf (atPeriods[0,1,2,3,4,5,6,7,8]) (insert $ cvMotif2)
-        ,  Leaf (atPeriods[3] .atMotifs [7]) (transp (-1))
-        ,  Node (atPeriods [4,5,6]) [
-              Leaf (atMotifs [0,1]) (transp (-4))
-           ,  Leaf (atMotifs [2,3,4,5,6,7]) (transp (-5))
-           ]
-        ]
-    , Leaf (atPeriods [8] . atMotifs[0]) (insert $ cMajor)
-    ]
-
--- need to combine multiplegroups and act on them like 1 !!
-
-cMajor = toGroup V [Note hn (C,4), Note hn (G,3)]
--- other version of "cellevevet"
-
-cv2 :: MusicPT
-cv2= Node (atPeriods [0,1,2,3,4,5,6,7]) [
-         Node (atVoices [0] ) [ --
-            Leaf (atMotifs [0,1,2,3,4,5,6,7]) (insert $ transp (-7) cvMotif)
-         ,  Leaf (atMotifs [2,3]) (transp (-2))
-         ,  Leaf (atMotifs [4,5]) (transp (-4))
-         ,  Leaf (atMotifs [1,3]) (mlSD (-1))
-         ,  Leaf (atMotifs [6]) (transp 1)
-         ,  Leaf (atMotifs [7]) (transp(-3))
-        ]
-        ,  Node (atPeriods [4,5]) [
-             Node (atVoices [0,1]) [
-             Leaf (atMotifs [0,1,2,3,4,5,6,7]) (transp (-3))
-             ]
-           , Node (atVoices [1]) [
-               Leaf (atMotifs [3,7]) (transp (-1))
-             , Leaf (atMotifs [2]) (transp(1))
-             ]
-          ]
-        , Leaf (atPeriods [8] . atMotifs[0]) (insert $ cMajor)
-        ]
-
--}
 chords :: OrientedTree (Primitive Pitch)
 chords =
               Group H [
@@ -274,7 +276,7 @@ makeStartingTree tis =
 
 toDefaultOrientedTree :: TreeShape -> MusicTree
 toDefaultOrientedTree =
-  go $ [Group V, Group H, Group H] ++ repeat (Group H) -- left is top
+  go $ repeat (Group H) -- left is top
  where
   go (c : cs) TLeaf      = Group H [Val $ Rest sn]
   go (c : cs) (TAll  t ) = c [go cs t]
