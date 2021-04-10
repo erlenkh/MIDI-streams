d4ea0b1fce1fe053a22302f6476278c67ae1ef71
erlenkh <erlenkh@stud.ntnu.no>
2021-04-10 18:27:15 +0200

    removed functions that used now deleted functions.

--- Main.hs
+++ Main.hs
@@ -3,18 +3,20 @@ import Composer
 import Structure
 import Transform
 import Euterpea
+
+-- just some tests for now.
+
 main = do
   gen <- newStdGen
-  let fp = Main.period gen motif4
   let newgen = snd $ m gen
-  let tranz = applyGT [Some[1,3]] (ro FA . giveR m2) . applyGT [Some[0,2]] (ro RI) . applyGT [All] (giveR m3 . toGroup H . fromGroup)
+  let tranz = applyGT [Some[1,3]] (ro FA) . applyGT [Some[0,2]] (ro RI) . applyGT [All] (toGroup H . flatten)
   print (tranz chords)
-  playDev 2 $ forever $ (treeToMusic $ (tranz) chords)  :=: (treeToMusic $ applyGT [All] (giveR m4) chords) -- :=: (treeToMusic $ fp)
+  playDev 2 $ forever $ (treeToMusic $ (tranz) chords)
 
-t = getRandom transformations
+--t = getRandom transformations
 m = getRandom motifs
 
-motif4 = Transform.transpose C Major (- 7) $ extend 1 motif
+motif4 = Transform.transpose C Major (- 7) $ fit 1 motif
 motif = [Note qn (C,4), Rest en, Note en (C,4), Note qn (A,3)]
 motif2 = [Note qn (C,4), Note qn (C,4), Note qn (B,4), Note qn (E,4)]
 motif3 = [Note sn (A,4), Note sn (A,4), Note qn (B,4), Note qn (C,4)]
@@ -28,20 +30,8 @@ m3 = toGroup H [Note hn (C,4), Note qn (D,4), Note qn (B,4)]
 m4 = toGroup H [Note wn (C,4), Note wn (D,4), Note wn (B,4)]
 base m = (Group H $ map (toGroup H) $  replicate 4 m) :: MusicTree
 
-period :: StdGen -> [Primitive Pitch] -> MusicTree
-period gen motif = applyGT [Some[1]] (weak . fst (t gen)) $
-  applyGT [Some[3]] (strong . fst (t gen) ) $
-  applyGT [Some[1,3]] (giveR m1) $
-   base motif
-
-section = (toGroup H motif)
-
-per gen =  applyGT [Some[1]] (weak . fst (t gen)) .
-  applyGT [Some[3]] (strong . fst (t gen) ) .
-  applyGT [Some[1,3]] (giveR m1)
 
 transformations = [inv, rev, transp (-1), transp 1, transp 3, transp(-2)]
-
 getRandom list seed =
   let randomIdx = randomR (0,(length list) -1) seed
   in (list !! fst randomIdx, snd randomIdx)
