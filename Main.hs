7d31a35f9eba109de50f3baccbcce2bfe39e7290
erlenkh <erlenkh@stud.ntnu.no>
2021-06-08 23:03:35 +0200

    writes to midi instead of streaming it. better for testing purposes..

--- Main.hs
+++ Main.hs
@@ -5,6 +5,11 @@ import System.Random
 
 main = do
   gen <- newStdGen
-  putStrLn $ show gen 
-  playDev 6 $ Example.genPiece gen
+  putStrLn $ show gen
+  writeMidi (midiPath $ show gen) $ Example.genPiece gen
+--  playDev 6 $ Example.genPiece gen
   -- |    ^ the midi channel you want to stream through
+
+midiPath name =
+  let folder = "/home/erlend/Documents/github/MIDI-streams/midi/"
+  in  folder ++ name ++ ".mid"
